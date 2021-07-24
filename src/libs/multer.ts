42f52ed136cc7e3e2fd2479168e38f340af6ca10
kensey1986 <myrc1986@gmail.com>
2021-07-24 10:39:21 -0500

    creacion de carpeta public

--- src/libs/multer.ts
+++ src/libs/multer.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 const storage: StorageEngine = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '/public/images')
+        cb(null, 'dist/public/images')
     },
     filename: function (req, file, cb) {
         cb(null, `${Date.now()}-${file.originalname}`)
