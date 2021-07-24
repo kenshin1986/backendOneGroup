8e4e0e80c2dfd88ea02bf341b871b28232b07f87
kensey1986 <myrc1986@gmail.com>
2021-07-24 10:01:05 -0500

    cambios package para deploy

--- src/libs/multer.ts
+++ src/libs/multer.ts
@@ -1,9 +1,10 @@
 import multer, { StorageEngine } from 'multer'
+import path from 'path'
 
 
 const storage: StorageEngine = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, `./public/images`)
+        cb(null, path.join(__dirname, '/public/images'))
     },
     filename: function (req, file, cb) {
         cb(null, `${Date.now()}-${file.originalname}`)
